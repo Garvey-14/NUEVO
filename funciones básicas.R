2ea8cd269e90c74d24edbf830c41732ba959bac8
Cristhian Hernández Martínez <chernandez162@alumnos.uaq.mx>
2024-03-06 10:19:48 -0600

    nivel de putez del garvey en función

--- funciones básicas.R
+++ funciones básicas.R
@@ -2,4 +2,9 @@ basado<-function(a,b){
   print(paste("La suma es",a+b))
 }
 
-MARZO<-C(1,2,3)
+MARZO<-c(1,2,3)
+garvey<-function(a){
+  print(paste("El nivel de putez del garvey es:",a ))
+}
+
+garvey(8)
